ab3036cac49283ef139a5d905a2a59b4f954f4b0
frasdor <frasunkiewicz.dorota@gmail.com>
2024-08-29 23:52:38 +0200

    aded task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,25 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputName.addEventListener('input',() => {
+    const trimmedValue = inputName.value.trim();
+    console.log('Trimmed value:', trimmedValue);
+
+    if(trimmedValue === '') {
+        return outputName.textContent = 'Anonymous';
+    } else {
+        return outputName.textContent = trimmedValue;
+    }
+});
+
+
+// zamiast if mozna uzyc operatora trojargumentowego nameOutput.textContent = trimmedValue ? trimmedValue : 'Anonymous';
+
+
+
 // Zadanie 3
 
 // Napisz skrypt, który podczas wprowadzania tekstu do inputu input#name-input (zdarzenie input) podstawia jego bieżącą wartość do span#name-output jako imię do powitania. Upewnij się, że wartość w inpucie jest oczyszczana z białych znaków na krańcach. Jeśli input jest pusty lub zawiera tylko spacje, to zamiast imienia w spanie powinien pojawić się ciąg "Anonymous".
-
-
-
-// <input type="text" id="name-input" placeholder="Please enter your name" />
-// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
-
-
 
 // Na co zwróci uwagę mentor podczas sprawdzania:
 
