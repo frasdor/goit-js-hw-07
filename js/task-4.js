959f769b392bb0dc5bf4020e1a3e2257901a10b3
frasdor <frasunkiewicz.dorota@gmail.com>
2024-08-30 10:39:13 +0200

    added task 4 and 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,38 @@
+const registerForm = document.querySelector('.login-form');
 
+registerForm.addEventListener("submit", event => {
+	event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+     
+    if (email === "" || password === "") {
+        return console.log('All form fields must be filled in');
+    }
+    const formData = { email, password }; // utworzono obiekt z danymi
+    console.log(formData); // wypisanie danych w konsoli
+        form.reset(); // zresetowanie formularza
+});
+
+
+
+
+
+// Zadanie 4
+
+// Napisz skrypt do zarządzania formularzem logowania.
+
+// Przetwarzanie wysyłania formularza form.login-form powinno odbywać się poprzez zdarzenie submit.
+// Podczas wysyłania formularza strona nie powinna być przeładowywana.
+// Jeśli podczas submit w formularzu są nieuzupełnione pola, wyświetl alert z komunikatem 'All form fields must be filled in'. Nie dodawaj atrybutu required do inputów, walidacja powinna odbywać się za pomocą JS.
+// Jeśli użytkownik wypełnił wszystkie pola i wysłał formularz, zbierz wartości pól do obiektu z dwoma właściwościami, gdzie kluczem jest nazwa inputów, a wartością są odpowiednie wartości tych inputów, oczyszczone z białych znaków na krawędziach. Do uzyskania dostępu do elementów formularza użyj właściwości elements.
+// Przy submit formularza wypisz obiekt z wprowadzonymi danymi w konsoli i zresetuj wartości pól formularza za pomocą metody reset.
+
+
+// Na co zwróci uwagę mentor podczas sprawdzania:
+
+// Nasłuchuje zdarzenia submit.
+// Podczas wysyłania formularza strona nie jest przeładowywana.
+// Jeśli podczas submit w formularzu są nieuzupełnione pola, wyświetla alert.
+// Przy submit formularza w konsoli wypisuje obiekt z dwoma właściwościami, gdzie klucze to nazwy inputów, a wartości to odpowiednie wartości tych inputów, oczyszczone z białych znaków na krawędziach.
+// Po submit formularza wartości pól są czyszczone.
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.body;
+
+
+changeColorBtn.addEventListener('click', () =>{
+  console.log('Activation of the background change function');
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+
+
+});
+
+
+
+// Zadanie 5
+
+// Napisz skrypt, który po kliknięciu na button.change-color zmienia kolor tła elementu <body> za pomocą stylu inline i przypisuje tę wartość koloru do span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Zauważ, że funkcja getRandomHexColor() zwraca kolor w formacie heksadecymalnym (hex), podczas gdy kolor tła na <body> będzie w formacie rgb. To jest normalne i nie wymaga żadnych poprawek.
+
+// Na co zwróci uwagę mentor podczas sprawdzania:
+
+// Tło na <body> jest ustawiane tylko po kliknięciu na button.change-color.
+// Przy każdym kliknięciu na button.change-color tło <body> jest malowane nowym losowym kolorem.
+// Na <body> i span.color wartości są tego samego koloru.
